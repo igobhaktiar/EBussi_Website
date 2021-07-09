3f4c1d64e7085957d2df2943b849481727bc1765
aqin23 <aqin.sid35@gmail.com>
2021-07-09 15:19:22 +0700

    update route

--- ebusi_v8/routes/web.php
+++ ebusi_v8/routes/web.php
@@ -25,3 +25,6 @@ Route::get('/home', function () {
 Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ADMIN
+Route::get('dashboard', [App\Http\Controllers\Backend\DashboardController::class, 'index']);
