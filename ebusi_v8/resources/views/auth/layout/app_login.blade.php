b24aabf61f906a91d462ed435326360a0ad18c2d
aqin23 <aqin.sid35@gmail.com>
2021-07-09 14:22:28 +0700

    template login

---
+++ ebusi_v8/resources/views/auth/layout/app_login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>E-BUSI</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<!-- MATERIAL DESIGN ICONIC FONT -->
+		<link rel="stylesheet" href="{{asset('loginn/fonts/material-design-iconic-font/css/material-design-iconic-font.min.css')}}">
+
+		<!-- STYLE CSS -->
+		<link rel="stylesheet" href="{{asset('loginn/css/style.css')}}">
+	</head>
+
+	<body>
+
+		<div class="wrapper" style="background-image: url('loginn/images/bg-registration-form-1.jpg');">
+    <div class="inner">
+        @yield('content')
+        
+      	</div>
+		
+	</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
