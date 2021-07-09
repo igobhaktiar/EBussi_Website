b9b652679d540bcf01d78b1f6c968a2fe58b3242
aqin23 <aqin.sid35@gmail.com>
2021-07-09 15:14:45 +0700

    Dashboard Admin

---
+++ ebusi_v8/resources/views/backend/layout2/sidebar.blade.php
@@ -0,0 +1,37 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+        <div class="sidebar-brand-icon">
+            <i class="fab fa-laravel"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">E-BUSI</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+  
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+    
+    <!-- <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-user"></i>
+            <span>User Dashboard</span></a>
+    </li> -->
+ 
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
