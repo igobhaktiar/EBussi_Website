4e796e99958d89d0b79961099151067bfd489168
YulinarNR <yulinarnurrahma@gmail.com>
2021-07-09 14:47:58 +0700

    update HomeController

--- ebusi_v8/app/Http/Controllers/HomeController.php
+++ ebusi_v8/app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Produk;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,7 +21,15 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index () {
-        return view ('welcome');
-        }
+    public function index()
+    {
+        //data barang yang akan ditampilkan dalam 1 halaman yakni ada 20 barang
+        //dengan menggunakan script (paginate)
+        $produks = Produk::paginate(20);
+        // untuk liat kasarannya
+        //  dd($produks); 
+        return view('welcome', compact('produks'));
+        
+        //compact digunkan untuk mengoper $produks ke view
+    }
 }
