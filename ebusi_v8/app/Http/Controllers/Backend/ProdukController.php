079897c7d6a95d9cb270d7ed4e0a8ddec851d79f
aqin23 <aqin.sid35@gmail.com>
2021-07-10 11:08:13 +0700

    Update Produk Controller

--- ebusi_v8/app/Http/Controllers/Backend/ProdukController.php
+++ ebusi_v8/app/Http/Controllers/Backend/ProdukController.php
@@ -15,4 +15,35 @@ class ProdukController extends Controller
         $produks = null;
         return view('backend.produk.create', compact('produks '));
         }
+
+        public function store(Request $request){
+            $input = $request->all();
+            $request->validate([
+                'nama_produk' => 'required|string|max:30',
+                'stok' => 'required',
+                'beratisi_produk' => 'required',
+                'harga_produk' => 'required',
+                'foto_produk' => 'required|image',
+                'keterangan' => 'required|string',          
+            ]);
+            
+            $imgName = $request->foto_produk->getClientOriginalName() . '-' . time() 
+                                   . '.' .   $request->foto_produk->extension();
+                                   
+            $request->foto_produk->move(public_path('uploads'), $imgName);
+    
+            Produk::create([
+                'nama_produk' => $request->nama_produk,
+                'kategori' => $request->kategori,
+                'stok' => $request->stok,
+                'beratisi_produk' => $request->beratisi_produk,
+                'harga_produk' => $request->harga_produk,
+                'foto_produk' => $imgName,
+                'keterangan' => $request->keterangan            
+            ]);
+            alert()->success('Data Produk berhasil disimpan', 'Success');
+            return redirect('index-read');
+            
+                          
+        }
 }
