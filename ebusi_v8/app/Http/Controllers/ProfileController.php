b5e5ff4114651be6f76ba2705a171632f74cb102
YulinarNR <yulinarnurrahma@gmail.com>
2021-07-12 01:49:51 +0700

    update user profile controller

--- ebusi_v8/app/Http/Controllers/ProfileController.php
+++ ebusi_v8/app/Http/Controllers/ProfileController.php
@@ -17,4 +17,25 @@ class ProfileController extends Controller
         $user = User::where('id', Auth::user()->id)->first();
         return view('profile_user.index', compact('user'));
     }
+    
+    public function update(Request $request){
+        $this->validate($request, [
+            'password' => 'confirmed',
+        ]);
+        $user = User::where('id', Auth::user()->id)->first();
+        $user->name = $request->name;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->nohp = $request->nohp;
+        $user->alamat = $request->alamat;
+        if(!empty($request->password)){
+            $user->password = Hash::make($request->password); 
+        }
+
+        $user->update();
+        alert()->success('Data profile sukses di update', 'Success');
+        return redirect('profile');
+        
+
+    }
 }
