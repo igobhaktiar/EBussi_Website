04c5f0e913b81a075686a3f15a8003d237b7fe23
YulinarNR <yulinarnurrahma@gmail.com>
2021-07-13 11:32:45 +0700

    update model produk

--- ebusi_v8/app/Models/Produk.php
+++ ebusi_v8/app/Models/Produk.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
+     // relasi saat ini buat tampilan katalog, SEMENTARA.
+    public function pesanan_detail(){
+        return $this->hasMany('App\Models\PesananDetail','produk_id', 'id');
+    }
+
     public function kategori(){
         // kategori itu berada di tabel produk yang di buat foreignkey
         return $this->belongsTo('App\Models\KategoriProduk','kategori', 'id');
