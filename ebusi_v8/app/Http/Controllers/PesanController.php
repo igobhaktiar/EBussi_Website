d95cab54047f9fa97119f0cb7c4242e1b0346075
YulinarNR <yulinarnurrahma@gmail.com>
2021-07-13 11:24:31 +0700

    update pesan controller

--- ebusi_v8/app/Http/Controllers/PesanController.php
+++ ebusi_v8/app/Http/Controllers/PesanController.php
@@ -18,4 +18,32 @@ class PesanController extends Controller
         $produk = Produk::where('id', $id)->first();
         return view('pesan.index', compact('produk'));
     }
+
+    public function pesan(Request $request, $id){
+        $produk = Produk::where('id', $id)->first();
+        $tanggal = Carbon::now();
+        
+        //validasi apakah melebihi stok
+        //ini sudah berfungsi
+        //tpi kalau untuk misalnya di keranjang udh ada 7 tpi pas di tmbh lagi 4 itu ga bisa woy
+        // masihh ke save. keadaan stoknya 10. kalau bingung, coba jalani aja.
+        if($request->jumlah_pesan >= $produk->stok){
+            alert()->warning('Jumlah produk melebihi stok, periksa keranjang anda', 'Warning');
+            return redirect('pesan/'.$id);
+        }
+
+        //cek validasi
+        $cek_pesanan = Pesanan::where('user_id', Auth::user()->id)->where('status',0)->first();
+        //simpan ke database pesanan
+        if(empty($cek_pesanan)){
+            $pesanan = new Pesanan;
+            $pesanan->user_id = Auth::user()->id ;
+            $pesanan->tanggal_pesanan = $tanggal;
+            $pesanan->status = 0;
+            $pesanan->jumlah_harga = 0;
+            $pesanan->save();
+            
+        }
+        
+    }
 }
