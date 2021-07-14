d4a5bdffd72933794432d26762b20150ab73582d
YulinarNR <yulinarnurrahma@gmail.com>
2021-07-15 06:02:03 +0700

    update controller dan memperbaiki nota

--- ebusi_v8/app/Http/Controllers/HistoryController.php
+++ ebusi_v8/app/Http/Controllers/HistoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Produk;
 use App\Models\Pesanan;
 use App\Models\PesananDetail;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use Carbon\Carbon;
 use Auth;
@@ -23,11 +24,30 @@ class HistoryController extends Controller
     }
 
     public function detail($id){
-        $user = User::where('id', Auth::user()->id)->first();
-        $pesanan = Pesanan::where('id', $id)->first();
-        $pesanan_details = PesananDetail::where('pesanan_id', $pesanan->id)->get();
+        // $user = User::where('id', Auth::user()->id)->first();
+        // $pesanan = Pesanan::where('id', $id)->first();
+        // $pesanan_details = PesananDetail::where('pesanan_id', $pesanan->id)->get();
         
-        return view('history.detail', compact('pesanan','pesanan_details'));
+        // return view('history.detail', compact('pesanan','pesanan_details'));
+        
+        $pesananDetail[] = [DB::table('pesanan_details')
+            ->Join(
+                'produks',
+                'pesanan_details.produk_id',
+                '=',
+                'produks.id',)
+                ->where('pesanan_details.pesanan_id', '=', $id)
+            ->get()];
+               
+        $dataUser = DB::table('users')
+                ->join('pesanans', 'pesanans.user_id', '=', 'users.id')
+                ->where('pesanans.id', '=', $id)
+                ->get();
+    //    dd($pesananDetail);
+        return view('history.detail',[
+            'pesananDetail' => $pesananDetail,
+            'dataUser' => $dataUser,
+        ]);
     }
 
 }
--- ebusi_v8/resources/views/history/detail.blade.php
+++ ebusi_v8/resources/views/history/detail.blade.php
@@ -17,17 +17,17 @@
                     <div class="form-row">
                   		<div class="col-md-4" style="text-align: justify;">
                   		  <h4>Pembelian</h4>
-                  		  <strong>No. Pembelian: AKI00{{$pesanan->id}}  </strong><br>
-                  		  Tanggal: {{$pesanan->tanggal_pesanan}}<br>
-                  		  Total: <label style="background-color: #FAEBD7;">Rp. {{number_format($pesanan->jumlah_harga)}}</label>
+                  		  <strong>No. Pembelian: AKI00{{$dataUser[0]->id}}  </strong><br>
+                  		  Tanggal: {{$dataUser[0]->tanggal_pesanan}}<br>
+                  		  Total: <label style="background-color: #FAEBD7;">Rp. {{number_format($dataUser[0]->jumlah_harga)}}</label>
 
                  		</div>
                      
                  		<div class="col-md-4" style="text-align: justify;">
                   		  <h4>Pelanggan</h4>
-                  		  <b>An. {{ Auth::user()->name }}</b> <br>
-                            {{ Auth::user()->nohp }} <br>
-                            {{ Auth::user()->email }}
+                  		  <b>An. {{$dataUser[0]->name}}</b> <br>
+                            {{$dataUser[0]->nohp }} <br>
+                            {{ $dataUser[0]->email }}
                   		  <p>
                   		  
                   		  </p>
@@ -35,26 +35,26 @@
 
                  		<div class="col-md-4">
                   		  <h4>Alamat COD</h4>
-                  		  {{ Auth::user()->alamat }} 
+                  		  {{$dataUser[0]->alamat }}
                  		</div>
                   	
                         <!-- <h5 class="m-0 font-weight-bold text-success">Detail Pemesanan</h5> -->
                         <!-- CARA NARUH KANAN TULISAN!!! -->
                         <!-- <h5 class="" text-align="right">djf</h5>     -->
                         </div><br>
-                        @if(!empty($pesanan))
+                        @if(!empty($dataUser[0]))
                         <p align="right">Status Pemesanan :  <br>
-                        @if ($pesanan->status == "1")
+                        @if ($dataUser[0]->status == "1")
                         <label style="background-color: #FAEBD7;">Menunggu Konfirmasi</label>
-                        @elseif ($pesanan->status == 2)
+                        @elseif ($dataUser[0]->status == 2)
                         <label style="background-color: #FAEBD7;">Pesanan Diproses</label>
-                        @elseif ($pesanan->status == 3)
+                        @elseif ($dataUser[0]->status == 3)
                         <label style="background-color: #FAEBD7;">Pesanan Dikirim</label>
-                    
+                        @endif
                          </p>
-                         @endif
+                    @endif
                       
-                    <!-- <p align="right">Tanggal Pesan : {{$pesanan->tanggal_pesanan}}</p> -->
+                    <!-- <p align="right">Tanggal Pesan : {{$dataUser[0]->tanggal_pesanan}}</p> -->
                         <table class="table table-bordered">
                             <thead>
                         <tr class="text-center">
@@ -69,31 +69,27 @@
                     </thead>
 
                     <tbody>
-                    <?php 
-                    $no=1; 
-                    ?>
-                    @foreach($pesanan_details as $pesanan_detail)
-                    <tr class="text-center">
-                        <td>{{$no++}}</td>
-                        <td>  <img src="{{ url('uploads')}}/{{$pesanan_detail->produk->foto_produk}}" width="50"></td>
-                        <td>{{ $pesanan_detail->produk->nama_produk}}</td>
-                        <!-- <td>Masih blm (catatan)</td> -->
-                        <td>{{ $pesanan_detail->jumlah}} ikat</td>
-                        <td>Rp. {{ number_format($pesanan_detail->produk->harga_produk)}}</td>
-                        <td>Rp. {{ number_format($pesanan_detail->jumlah_harga)}}</td>
-                    </tr>
-                    @endforeach
+                    @foreach($pesananDetail[0][0] as $detail )
+                          <tr class="text-center">
+                            <td>{{$loop->iteration}}</td>
+                            <td class="text-center"> <img src="{{ url('uploads') }}/{{$detail->foto_produk}}" width="40" ></td>
+                            <td>{{$detail->nama_produk}}</td>
+                            <td>{{$detail->jumlah}}</td>
+                            <td>Rp. {{number_format($detail->harga_produk)}}</td>
+                            <td>Rp. {{number_format($detail->jumlah_harga)}}</td>
+                          
+                          </tr>
+                          @endforeach
 
                     <tr align="center">
                         <th colspan="5" style="text-align: left;">Total Belanja</th>
-                        <th  align="center">Rp. {{number_format($pesanan->jumlah_harga)}}</th>
+                        <th  align="center">Rp. {{number_format($dataUser[0]->jumlah_harga)}}</th>
                     </tr>
                     <tr>
                     <!-- <th colspan="5" style="text-align: left;"> <a href="{{ url ('pesan-batal')}}" class="btn btn-danger btn-sm">Batalkan Pesanan</i></a></th> -->
                     </tr>
                     </tbody>
                 </table>
-                @endif
             </div>
         </div>
                
